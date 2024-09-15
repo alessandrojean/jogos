8cb4ea84054541f5bd716cbefec054025062cae7
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2024-09-15 17:03:12 -0300

    refactor: move icons folder

--- src/model/certification.ts
+++ src/model/certification.ts
@@ -58,33 +58,33 @@ export class Certification extends GObject.Object {
 
 export const certifications: Certification[] = [
   /* Entertainment Software Rating Board (ESRB) */
-  new Certification({ id: 'ESRB_E', system: 'ESRB', name: 'Everyone', iconName: 'esrb-everyone' }),
-  new Certification({ id: 'ESRB_E10', system: 'ESRB', name: 'Everyone 10+', iconName: 'esrb-everyone-10-plus' }),
-  new Certification({ id: 'ESRB_T', system: 'ESRB', name: 'Teen', iconName: 'esrb-teen' }),
-  new Certification({ id: 'ESRB_M17', system: 'ESRB', name: 'Mature', iconName: 'esrb-mature' }),
-  new Certification({ id: 'ESRB_AO', system: 'ESRB', name: 'Adults Only', iconName: 'esrb-adults-only' }),
+  new Certification({ id: 'ESRB_E', system: 'ESRB', name: 'Everyone', iconName: 'certification-esrb-everyone' }),
+  new Certification({ id: 'ESRB_E10', system: 'ESRB', name: 'Everyone 10+', iconName: 'certification-esrb-everyone-10-plus' }),
+  new Certification({ id: 'ESRB_T', system: 'ESRB', name: 'Teen', iconName: 'certification-esrb-teen' }),
+  new Certification({ id: 'ESRB_M17', system: 'ESRB', name: 'Mature', iconName: 'certification-esrb-mature' }),
+  new Certification({ id: 'ESRB_AO', system: 'ESRB', name: 'Adults Only', iconName: 'certification-esrb-adults-only' }),
 
   /* Pan-European Game Information (PEGI) */
-  new Certification({ id: 'PEGI_3', system: 'PEGI', name: 'PEGI 3', iconName: 'pegi-3-years' }),
-  new Certification({ id: 'PEGI_7', system: 'PEGI', name: 'PEGI 7', iconName: 'pegi-7-years' }),
-  new Certification({ id: 'PEGI_12', system: 'PEGI', name: 'PEGI 12', iconName: 'pegi-12-years' }),
-  new Certification({ id: 'PEGI_16', system: 'PEGI', name: 'PEGI 16', iconName: 'pegi-16-years' }),
-  new Certification({ id: 'PEGI_18', system: 'PEGI', name: 'PEGI 18', iconName: 'pegi-18-years' }),
+  new Certification({ id: 'PEGI_3', system: 'PEGI', name: 'PEGI 3', iconName: 'certification-pegi-3-years' }),
+  new Certification({ id: 'PEGI_7', system: 'PEGI', name: 'PEGI 7', iconName: 'certification-pegi-7-years' }),
+  new Certification({ id: 'PEGI_12', system: 'PEGI', name: 'PEGI 12', iconName: 'certification-pegi-12-years' }),
+  new Certification({ id: 'PEGI_16', system: 'PEGI', name: 'PEGI 16', iconName: 'certification-pegi-16-years' }),
+  new Certification({ id: 'PEGI_18', system: 'PEGI', name: 'PEGI 18', iconName: 'certification-pegi-18-years' }),
 
   /* Computer Entertainment Rating Organization (CERO) */
-  new Certification({ id: 'CERO_A', system: 'CERO', name: 'CERO A', iconName: 'cero-a' }),
-  new Certification({ id: 'CERO_B', system: 'CERO', name: 'CERO B', iconName: 'cero-b' }),
-  new Certification({ id: 'CERO_C', system: 'CERO', name: 'CERO C', iconName: 'cero-c' }),
-  new Certification({ id: 'CERO_D', system: 'CERO', name: 'CERO D', iconName: 'cero-d' }),
-  new Certification({ id: 'CERO_Z', system: 'CERO', name: 'CERO Z', iconName: 'cero-z' }),
+  new Certification({ id: 'CERO_A', system: 'CERO', name: 'CERO A', iconName: 'certification-cero-a' }),
+  new Certification({ id: 'CERO_B', system: 'CERO', name: 'CERO B', iconName: 'certification-cero-b' }),
+  new Certification({ id: 'CERO_C', system: 'CERO', name: 'CERO C', iconName: 'certification-cero-c' }),
+  new Certification({ id: 'CERO_D', system: 'CERO', name: 'CERO D', iconName: 'certification-cero-d' }),
+  new Certification({ id: 'CERO_Z', system: 'CERO', name: 'CERO Z', iconName: 'certification-cero-z' }),
 
   /* Classificação Indicativa (ClassInd) */
-  new Certification({ id: 'CLASSIND_L', system: 'CLASSIND', name: 'Livre', iconName: 'classind-livre' }),
-  new Certification({ id: 'CLASSIND_10', system: 'CLASSIND', name: '10 anos', iconName: 'classind-10-anos' }),
-  new Certification({ id: 'CLASSIND_12', system: 'CLASSIND', name: '12 anos', iconName: 'classind-12-anos' }),
-  new Certification({ id: 'CLASSIND_14', system: 'CLASSIND', name: '14 anos', iconName: 'classind-14-anos' }),
-  new Certification({ id: 'CLASSIND_16', system: 'CLASSIND', name: '16 anos', iconName: 'classind-16-anos' }),
-  new Certification({ id: 'CLASSIND_18', system: 'CLASSIND', name: '18 anos', iconName: 'classind-18-anos' }),
+  new Certification({ id: 'CLASSIND_L', system: 'CLASSIND', name: 'Livre', iconName: 'certification-classind-livre' }),
+  new Certification({ id: 'CLASSIND_10', system: 'CLASSIND', name: '10 anos', iconName: 'certification-classind-10-anos' }),
+  new Certification({ id: 'CLASSIND_12', system: 'CLASSIND', name: '12 anos', iconName: 'certification-classind-12-anos' }),
+  new Certification({ id: 'CLASSIND_14', system: 'CLASSIND', name: '14 anos', iconName: 'certification-classind-14-anos' }),
+  new Certification({ id: 'CLASSIND_16', system: 'CLASSIND', name: '16 anos', iconName: 'certification-classind-16-anos' }),
+  new Certification({ id: 'CLASSIND_18', system: 'CLASSIND', name: '18 anos', iconName: 'certification-classind-18-anos' }),
 ]
 
 const certificationMap = Object.fromEntries(certifications.map(p => [p.id, p])) as Record<CertificationId, Certification>
--- src/model/platform.ts
+++ src/model/platform.ts
@@ -58,46 +58,46 @@ export class Platform extends GObject.Object {
 }
 
 export const platforms: Platform[] = [
-  new Platform({ id: 'MAC_OS', name: 'macOS', iconName: 'mac-os', generation: -1 }),
-  new Platform({ id: 'WINDOWS', name: 'Windows', iconName: 'windows', generation: -1 }),
+  new Platform({ id: 'MAC_OS', name: 'macOS', iconName: 'platform-mac-os-symbolic', generation: -1 }),
+  new Platform({ id: 'WINDOWS', name: 'Windows', iconName: 'platform-windows-symbolic', generation: -1 }),
 
-  new Platform({ id: 'PLAYSTATION_5', name: 'PlayStation 5', iconName: 'playstation', generation: 9 }),
-  new Platform({ id: 'XBOX_SERIES', name: 'Xbox Series', iconName: 'xbox', generation: 9 }),
+  new Platform({ id: 'PLAYSTATION_5', name: 'PlayStation 5', iconName: 'platform-playstation-symbolic', generation: 9 }),
+  new Platform({ id: 'XBOX_SERIES', name: 'Xbox Series', iconName: 'platform-xbox-symbolic', generation: 9 }),
 
-  new Platform({ id: 'NINTENDO_3DS', name: 'Nintendo 3DS', iconName: 'nintendo-3ds', generation: 8 }),
-  new Platform({ id: 'NINTENDO_SWITCH', name: 'Nintendo Switch', iconName: 'nintendo-switch', generation: 8 }),
-  new Platform({ id: 'NINTENDO_WII_U', name: 'Nintendo Wii U', iconName: 'wii-u', generation: 8 }),
-  new Platform({ id: 'PLAYSTATION_4', name: 'PlayStation 4', iconName: 'playstation', generation: 8 }),
-  new Platform({ id: 'PLAYSTATION_VITA', name: 'PlayStation Vita', iconName: 'playstation', generation: 8 }),
-  new Platform({ id: 'XBOX_ONE', name: 'Xbox One', iconName: 'xbox', generation: 8 }),
+  new Platform({ id: 'NINTENDO_3DS', name: 'Nintendo 3DS', iconName: 'platform-nintendo-3ds-symbolic', generation: 8 }),
+  new Platform({ id: 'NINTENDO_SWITCH', name: 'Nintendo Switch', iconName: 'platform-nintendo-switch-symbolic', generation: 8 }),
+  new Platform({ id: 'NINTENDO_WII_U', name: 'Nintendo Wii U', iconName: 'platform-wii-u-symbolic', generation: 8 }),
+  new Platform({ id: 'PLAYSTATION_4', name: 'PlayStation 4', iconName: 'platform-playstation-symbolic', generation: 8 }),
+  new Platform({ id: 'PLAYSTATION_VITA', name: 'PlayStation Vita', iconName: 'platform-playstation-symbolic', generation: 8 }),
+  new Platform({ id: 'XBOX_ONE', name: 'Xbox One', iconName: 'platform-xbox-symbolic', generation: 8 }),
 
-  new Platform({ id: 'NINTENDO_DS', name: 'Nintendo DS', iconName: 'nintendo-ds', generation: 7 }),
-  new Platform({ id: 'NINTENDO_WII', name: 'Nintendo Wii', iconName: 'wii', generation: 7 }),
-  new Platform({ id: 'PLAYSTATION_3', name: 'PlayStation 3', iconName: 'playstation', generation: 7 }),
-  new Platform({ id: 'PSP', name: 'PSP', iconName: 'playstation', generation: 7 }),
-  new Platform({ id: 'XBOX_360', name: 'Xbox 360', iconName: 'xbox', generation: 7 }),
+  new Platform({ id: 'NINTENDO_DS', name: 'Nintendo DS', iconName: 'platform-nintendo-ds-symbolic', generation: 7 }),
+  new Platform({ id: 'NINTENDO_WII', name: 'Nintendo Wii', iconName: 'platform-wii-symbolic', generation: 7 }),
+  new Platform({ id: 'PLAYSTATION_3', name: 'PlayStation 3', iconName: 'platform-playstation-symbolic', generation: 7 }),
+  new Platform({ id: 'PSP', name: 'PSP', iconName: 'platform-playstation-symbolic', generation: 7 }),
+  new Platform({ id: 'XBOX_360', name: 'Xbox 360', iconName: 'platform-xbox-symbolic', generation: 7 }),
 
-  new Platform({ id: 'DREAMCAST', name: 'Dreamcast', iconName: 'dreamcast', generation: 6 }),
-  new Platform({ id: 'GAME_BOY_ADVANCE', name: 'Game Boy Advance', iconName: 'nintendo-game-boy-advance', generation: 6 }),
-  new Platform({ id: 'GAMECUBE', name: 'GameCube', iconName: 'gamecube', generation: 6 }),
-  new Platform({ id: 'PLAYSTATION_2', name: 'PlayStation 2', iconName: 'playstation', generation: 6 }),
-  new Platform({ id: 'XBOX', name: 'Xbox', iconName: 'xbox', generation: 6 }),
+  new Platform({ id: 'DREAMCAST', name: 'Dreamcast', iconName: 'platform-dreamcast-symbolic', generation: 6 }),
+  new Platform({ id: 'GAME_BOY_ADVANCE', name: 'Game Boy Advance', iconName: 'platform-nintendo-game-boy-advance-symbolic', generation: 6 }),
+  new Platform({ id: 'GAMECUBE', name: 'GameCube', iconName: 'platform-gamecube-symbolic', generation: 6 }),
+  new Platform({ id: 'PLAYSTATION_2', name: 'PlayStation 2', iconName: 'platform-playstation-symbolic', generation: 6 }),
+  new Platform({ id: 'XBOX', name: 'Xbox', iconName: 'platform-xbox-symbolic', generation: 6 }),
 
-  new Platform({ id: 'ATARI_JAGUAR', name: 'Atari Jaguar', iconName: 'atari-jaguar', generation: 5 }),
-  new Platform({ id: 'GAME_BOY_COLOR', name: 'Game Boy Color', iconName: 'nintendo-game-boy', generation: 5 }),
-  new Platform({ id: 'NINTENDO_64', name: 'Nintendo 64', iconName: 'nintendo-64', generation: 5 }),
-  new Platform({ id: 'PLAYSTATION', name: 'PlayStation', iconName: 'playstation', generation: 5 }),
-  new Platform({ id: 'SATURN', name: 'Saturn', iconName: 'sega-saturn', generation: 5 }),
+  new Platform({ id: 'ATARI_JAGUAR', name: 'Atari Jaguar', iconName: 'platform-atari-jaguar-symbolic', generation: 5 }),
+  new Platform({ id: 'GAME_BOY_COLOR', name: 'Game Boy Color', iconName: 'platform-nintendo-game-boy-symbolic', generation: 5 }),
+  new Platform({ id: 'NINTENDO_64', name: 'Nintendo 64', iconName: 'platform-nintendo-64-symbolic', generation: 5 }),
+  new Platform({ id: 'PLAYSTATION', name: 'PlayStation', iconName: 'platform-playstation-symbolic', generation: 5 }),
+  new Platform({ id: 'SATURN', name: 'Saturn', iconName: 'platform-sega-saturn-symbolic', generation: 5 }),
 
-  new Platform({ id: 'GAME_BOY', name: 'Game Boy', iconName: 'nintendo-game-boy', generation: 4 }),
-  new Platform({ id: 'MEGA_DRIVE', name: 'Mega Drive', iconName: 'sega-mega-drive', generation: 4 }),
-  new Platform({ id: 'SUPER_NINTENDO', name: 'Super Nintendo', iconName: 'snes', generation: 4 }),
+  new Platform({ id: 'GAME_BOY', name: 'Game Boy', iconName: 'platform-nintendo-game-boy-symbolic', generation: 4 }),
+  new Platform({ id: 'MEGA_DRIVE', name: 'Mega Drive', iconName: 'platform-sega-mega-drive-symbolic', generation: 4 }),
+  new Platform({ id: 'SUPER_NINTENDO', name: 'Super Nintendo', iconName: 'platform-snes-symbolic', generation: 4 }),
 
-  new Platform({ id: 'ATARI_7800', name: 'Atari 7800', iconName: 'atari', generation: 3 }),
-  new Platform({ id: 'MASTER_SYSTEM', name: 'Master System', iconName: 'sega-master-system', generation: 3 }),
-  new Platform({ id: 'NES', name: 'NES', iconName: 'nes', generation: 3 }),
+  new Platform({ id: 'ATARI_7800', name: 'Atari 7800', iconName: 'platform-atari-symbolic', generation: 3 }),
+  new Platform({ id: 'MASTER_SYSTEM', name: 'Master System', iconName: 'platform-sega-master-system-symbolic', generation: 3 }),
+  new Platform({ id: 'NES', name: 'NES', iconName: 'platform-nes-symbolic', generation: 3 }),
 
-  new Platform({ id: 'ATARI_2600', name: 'Atari 2600', iconName: 'atari', generation: 2 }),
+  new Platform({ id: 'ATARI_2600', name: 'Atari 2600', iconName: 'platform-atari-symbolic', generation: 2 }),
 ]
 
 const platformMap = Object.fromEntries(platforms.map(p => [p.id, p])) as Record<PlatformId, Platform>
